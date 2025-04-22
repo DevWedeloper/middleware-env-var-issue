591b6bd00af6f75be119be4b5b643c9f709b329b
DevWedeloper <vicnathangabrielle@gmail.com>
2025-04-22 12:11:34 +0800

    fix: server environment variable is undefined
    In development, middleware for client routes runs through Vite's dev
    server, which does not expose server-only variables on import.meta.env.
    Use process.env in the middleware instead to access the server
    variables. It is defined in development, and when the server is running
    with the defined environment variables.

--- src/server/middleware/auth.ts
+++ src/server/middleware/auth.ts
@@ -4,10 +4,10 @@ export default defineEventHandler((event) => {
   console.log('Path:', getRequestURL(event).pathname);
   console.log(
     'Server Only Environment Variable:',
-    import.meta.env['SERVER_ONLY_VARIABLE']
+    process.env['SERVER_ONLY_VARIABLE']
   );
   console.log(
     'Public Environment Variable:',
-    import.meta.env['VITE_EXAMPLE_VARIABLE']
+    process.env['VITE_EXAMPLE_VARIABLE']
   );
 });
